6587f507b1433a425245863aaa6e1a4e9d24cf42
Kunsang Tsering <kt@reflect-media.de>
2018-01-15 13:04:48 +0100

    fix partnerwinzer related posts

--- single-rebsorte.php
+++ single-rebsorte.php
@@ -91,7 +91,10 @@ get_header(); ?>
                         'value' => '"' . get_the_ID() . '"',
                         'compare' => 'LIKE'
                         )
-                    )
+                    ),
+                    'meta_key' => 'reihenfolge', 
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC'
                 ));
               if( $partners ): ?>
 
